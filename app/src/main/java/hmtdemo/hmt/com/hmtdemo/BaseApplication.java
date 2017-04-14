7b34304975761e4adb171632d86631d1ce1d2d07
renbo <renbo.ma@hypers.com>
2017-04-14 16:10:12 +0800

    update

---
+++ app/src/main/java/hmtdemo/hmt/com/hmtdemo/BaseApplication.java
@@ -0,0 +1,22 @@
+package hmtdemo.hmt.com.hmtdemo;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * Created by renbo on 2017/4/14.
+ */
+
+public class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+    }
+}
