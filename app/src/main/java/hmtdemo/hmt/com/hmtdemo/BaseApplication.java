4b3f7c1ccb616ebf4a7253e12b3eaabb4b0379a3
renbo <renbo.ma@hypers.com>
2017-05-04 18:33:04 +0800

    update

--- app/src/main/java/hmtdemo/hmt/com/hmtdemo/BaseApplication.java
+++ app/src/main/java/hmtdemo/hmt/com/hmtdemo/BaseApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.squareup.leakcanary.LeakCanary;
 
+import cn.com.iresearch.phonemonitor.irssdk.IRSSDK;
+
 /**
  * Created by renbo on 2017/4/14.
  */
@@ -18,5 +20,22 @@ public class BaseApplication extends Application {
             return;
         }
         LeakCanary.install(this);
+
+        buildSDK();
+        getIRSSDK().start();
+    }
+
+    private IRSSDK mIRSSDK;
+
+    private void buildSDK() {
+        mIRSSDK = new IRSSDK.Builder(getApplicationContext())
+                .build();
+    }
+
+    public IRSSDK getIRSSDK() {
+        if (mIRSSDK == null) {
+            buildSDK();
+        }
+        return mIRSSDK;
     }
 }
